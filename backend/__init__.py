96c98624365dcfa1804f426a825197b2237f27aa
SillyPasty <47266819+SillyPasty@users.noreply.github.com>
2020-07-01 17:40:37 +0800

    add cors

--- backend/__init__.py
+++ backend/__init__.py
@@ -2,9 +2,12 @@ from flask import Flask
 from backend.config import FlaskConfig
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_cors import CORS
 
 flask_app = Flask(__name__)
 flask_app.config.from_object(FlaskConfig)
+
+CORS(flask_app)
 
 db_app = SQLAlchemy(flask_app)
 migrate = Migrate(flask_app, db_app)
