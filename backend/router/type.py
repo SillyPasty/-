5bd4ab3b9982181a3966a226254af8a72641d3eb
SillyPasty <47266819+SillyPasty@users.noreply.github.com>
2020-07-02 22:16:54 +0800

    v

--- backend/router/type.py
+++ backend/router/type.py
@@ -8,7 +8,7 @@ type_bp = Blueprint('type', __name__)
 @type_bp.route('/api/type', methods=['GET'])
 def get_type():
     args_dic = {}
-    f_type = request.args.get('first_type', type=str)
+    f_type = request.args.get('first_type')
     if f_type != None:
         args_dic['type1'] = f_type
     types = Type.get_type(args_dic)
